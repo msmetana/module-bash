53f78cddb686d209b6e49042484bffd11593d2ec
Maks <smetana.maksim@gmail.com>
2018-06-14 16:01:51 +0300

    ex03

---
+++ ex03/ex03.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+  exit 1
+fi
+
+for param in $@
+do
+  if [ $param == $1 ]
+  then
+    continue
+  fi
+  echo "$param $(grep $param $1 -c)"
+  grep -n "$param" $1 | cut -d : -f 1
+done
+
